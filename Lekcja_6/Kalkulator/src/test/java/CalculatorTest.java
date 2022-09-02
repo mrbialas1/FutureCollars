456e49dfae1fd58e3f8aa5905cd13174d918e49d
Piotr Białas <mrbialas123@gmail.com>
2022-09-02 12:56:16 +0200

    Poprawione zadanie 2

--- Lekcja_6/Kalkulator/src/test/java/CalculatorTest.java
+++ Lekcja_6/Kalkulator/src/test/java/CalculatorTest.java
@@ -6,22 +6,42 @@ public class CalculatorTest {
 
     @Test
     void shouldAddTwoValues1and1() {
-        double sum = Calculator.sum(1, 1);
+        //given
+        int number1 = 1;
+        int number2 = 1;
+        //when
+        double sum = Calculator.sum(number1,number2);
+        //then
         Assertions.assertEquals(2, sum);
     }
     @Test
     void shouldSubtractTwoValues1and1() {
-        double sub = Calculator.sub(1, 1);
+        //given
+        int number1 = 1;
+        int number2 = 1;
+        //when
+        double sub = Calculator.sub(number1,number2);
+        //then
         Assertions.assertEquals(0, sub);
     }
     @Test
     void shouldMultiplyTwoValues1and1() {
-        double mul = Calculator.mul(1, 1);
+        //given
+        int number1 = 1;
+        int number2 = 1;
+        //when
+        double mul = Calculator.mul(number1,number2);
+        //then
         Assertions.assertEquals(1, mul);
     }
     @Test
     void shouldDivideTwoValues1and1() {
-        double div = Calculator.div(1, 1);
+        //given
+        int number1 = 1;
+        int number2 = 1;
+        //when
+        double div = Calculator.div(number1,number2);
+        //then
         Assertions.assertEquals(1, div);
     }
 }
